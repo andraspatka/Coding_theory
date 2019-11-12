558dd58221b91e5f9fb7fd01f245e4c6eb507fa6
Patka Zsolt-Andras <andras.patka@gmail.com>
2019-11-12 22:25:53 +0200

    Started implementind Shannon Fano

--- src/shannon_fano.py
+++ src/shannon_fano.py
@@ -3,4 +3,40 @@ import stats
 
 def shannonFanno(fileName):
     stat = stats.createStatistic(fileName)
-    print(stat)
+    codes = [list(tup + ('',)) for tup in stat]
+
+    start = 0
+    while True:
+        part = indexToPartAt(stat, start)
+        if start >= part:
+            break
+        for i in range(start, len(stat)):
+            print(str(i) + ' f1')
+            code = '0' if i <= part else '1'
+            codes[i][3] = codes[i][3] + code
+        for i in range(start, part):
+            print(i)
+            code = '0' if i == start else '1'
+            codes[i][3] = codes[i][3] + code
+        start = part + 1
+        #print(part)
+
+    print(codes)
+
+def indexToPartAt(stat, start):
+    topSum = 0
+    topIndex = 0
+    bottomSum = 0
+    bottomIndex = 0
+    desired = 0
+    for i in range(start, len(stat)):
+        desired += stat[i][2]
+    desired /= 2
+    for i in range(start, len(stat)):
+        if topSum < desired:
+            topIndex = i
+            topSum += stat[topIndex][2]
+        if bottomSum < desired:
+            bottomIndex = len(stat) - i - 1 
+            bottomSum += stat[bottomIndex][2]
+    return bottomIndex if ((bottomSum - desired) < (topSum - desired)) else topIndex
--- src/stats.py
+++ src/stats.py
@@ -6,12 +6,12 @@ import collections
 :param fileName: path to the input file
 
 :returns: 
-    A dictionary containing the symbols and their number of occurences in the input file.
+    A dictionary containing the symbols, their number of occurences and the percentage in the input file.
     The dictionary is sorted based on the character occurences in descending order.
 """
 def createStatistic(filePath):
     stat = {}
-
+    charNum = 0
     with open(filePath, "r") as f:
         f = open(filePath, "r")
         line = f.readline()
@@ -22,20 +22,17 @@ def createStatistic(filePath):
                     stat[c] = 1
                 else:
                     stat[c] = stat[c] + 1
+                charNum += 1
             line = f.readline()
-    return sorted(stat.items(), key=operator.itemgetter(1), reverse=True)
+    stat = sorted(stat.items(), key=operator.itemgetter(1), reverse=True)
+    return [tup + (tup[1]/charNum * 100,) for tup in stat]
 
 """Displays the values stored in a dictionary
-:param stat: dictionary containing symbols and their number of appearances
+:param stat: dictionary containing symbols, their number of appearances and the percentage
 """    
 def displayStatistic(stat):
-    charNum = 0
-    for elem in stat:
-        charNum += elem[1]
-    print("Total number of characters: " + str(charNum))
     with open('out.txt', 'w') as f:
         for elem in stat:
-            p = elem[1] / charNum * 100
-            line = f"'{elem[0]}': {elem[1]} {p:.2f}%"
+            line = f"'{elem[0]}': {elem[1]} {elem[2]:.2f}%"
             print(line)
             f.write(line + "\n")
